5eb7c7f0b0ba33fe28552765bbf86aefbe48d6a4
otsha <32258613+otsha@users.noreply.github.com>
2020-01-23 10:44:43 +0200

    Restaurant url is shown along with its name
    Quick styling fix to link

--- src/components/Randomizer.js
+++ src/components/Randomizer.js
@@ -3,22 +3,26 @@ import { Button } from 'react-bootstrap'
 import './Randomizer.css'
 
 const Randomizer = ({ restaurantService }) => {
-  const [restaurantName, setRestaurantName] = useState('Press the button')
+  const [restaurant, setRestaurant] = useState('Press the button')
 
   const changeRestaurantHandler = async (event) => {
     event.preventDefault()
     const restaurants = await restaurantService.getAll()
     if (restaurants && restaurants.length > 0) {
-      const newRestaurant = restaurants[Math.floor(Math.random() * restaurants.length)].name
-      setRestaurantName(newRestaurant)
+      const newRestaurant = restaurants[Math.floor(Math.random() * restaurants.length)]
+      setRestaurant(newRestaurant)
     } else {
-      setRestaurantName('Sorry, No restaurants available :C')
+      setRestaurant({ name: 'Sorry, No restaurants available :C' })
     }
   }
 
   return (
     <div data-testid='randomizer' className='randomizer'>
-      <h1 data-testid='randomizer-resultLabel'>{restaurantName}</h1>
+      <h1 data-testid='randomizer-resultLabel'>{restaurant.name}</h1>
+      {restaurant.url
+        ? <p><a data-testid='randomizer-restaurantUrl' href={restaurant.url}>Website</a></p>
+        : <></>
+      }
       <Button data-testid='randomizer-randomizeButton' onClick={changeRestaurantHandler} variant='success' size='lg'>Go!</Button>
     </div>
   )
