4e8479f96cb708defdca1353f5dec2877cb50e2b
Ilari Tommiska <ilari97@hotmail.com>
2020-01-23 12:41:25 +0200

    Fixed direct urls not allowing navigating to pages

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use('/api/restaurants', restaurantsRouter)
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(config.staticDir, 'index.html'))
 })
 
