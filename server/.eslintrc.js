cbe73272de8b02c9fd87cc17400579c4aed8569d
Ilari Tommiska <ilari97@hotmail.com>
2020-01-23 10:43:01 +0200

    Added eslint

---
+++ server/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'extends': '../.eslintrc.js',
+  'env': {
+    'node': true,
+    'browser': false,
+    'es6': false
+  },
+  'parserOptions': {
+    'sourceType': 'script'
+  },
+  'plugins': []
+}
