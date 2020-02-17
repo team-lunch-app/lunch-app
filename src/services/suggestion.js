cb5736d50da0ecb302444d6738011033206c3841
Ilari Tommiska <ilari97@hotmail.com>
2020-02-17 13:51:50 +0200

    Added service for suggestions

---
+++ src/services/suggestion.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+import authService from './authentication'
+const baseUrl = '/api/suggestions'
+
+const getAll = async () => {
+  const response = await axios.get(`${baseUrl}`, { headers: { authorization: `bearer ${authService.getToken()}` } })
+  return response.data
+}
+
+const approve = async (id) => {
+  const response = await axios.post(`${baseUrl}/approve/${id}`, { headers: { authorization: `bearer ${authService.getToken()}` } })
+  return response.data
+}
+
+const reject = async (id) => {
+  const response = await axios.post(`${baseUrl}/reject/${id}`, { headers: { authorization: `bearer ${authService.getToken()}` } })
+  return response.data
+}
+
+const addRestaurant = async (restaurant) => {
+  const response = await axios.post(`${baseUrl}/add`, restaurant)
+  return response.data
+}
+
+const removeRestaurant = async (restaurant) => {
+  const response = await axios.post(`${baseUrl}/remove`, restaurant)
+  return response.data
+}
+
+export default {
+  getAll,
+  approve,
+  reject,
+  addRestaurant,
+  removeRestaurant
+}
