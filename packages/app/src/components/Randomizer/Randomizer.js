a3731ce459e7f7ab8b284d17ca4394e4ae2f9b8a
otsha <32258613+otsha@users.noreply.github.com>
2020-04-02 14:13:35 +0300

    Add functionality to approve a lottery result

--- packages/app/src/components/Randomizer/Randomizer.js
+++ packages/app/src/components/Randomizer/Randomizer.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { Button } from 'react-bootstrap'
-import { ExpandMore, ExpandLess } from '@material-ui/icons'
+import { ExpandMore, ExpandLess, ThumbUpAlt, ThumbDownAlt } from '@material-ui/icons'
 
 import RestaurantEntry from '../RestaurantEntry/RestaurantEntry'
 import Filter from '../Filter/Filter/Filter'
@@ -30,6 +30,7 @@ const Randomizer = ({
   const [restaurant, setRestaurant] = useState()
   const [iteration, setIteration] = useState(maxNumberOfRolls)
   const [isRolling, setRolling] = useState(false)
+  const [resultSelected, setResultSelected] = useState(false)
 
   const [filter, setFilter] = useState({
     type: 'some',
@@ -71,9 +72,16 @@ const Randomizer = ({
       : time
   }
 
+  const handleApproveResult = async () => {
+    if (restaurant && restaurant.id) {
+      await restaurantService.increaseSelectedAmount(restaurant.id)
+      setRestaurant({ ...restaurant, selectedAmount: restaurant.selectedAmount + 1 })
+      setResultSelected(true)
+    }
+  }
+
   const startRolling = async () => {
     if (restaurant && restaurant.id) {
-
       await restaurantService.increaseNotSelectedAmount(restaurant.id)
     }
     const restaurants = await restaurantService.getAllMatches(filter.type, filter.categories, filter.distance)
@@ -122,7 +130,19 @@ const Randomizer = ({
         ? <h1 data-testid='roll-label'>{restaurant.name}</h1>
         : <>
           <Confetti />
+          {resultSelected && <h1>No backing down now! You&apos;re having lunch at:</h1>}
           <RestaurantEntry restaurant={restaurant} showMap />
+          {!resultSelected &&
+            <Button
+              data-testid='randomizer-approveButton'
+              className='randomizer-approveButton'
+              onClick={handleApproveResult}
+              variant='success'
+              size='lg'>
+              <span>
+                <ThumbUpAlt /> Ok, I&apos;m picking this one!
+              </span>
+            </Button>}
         </>
       : <h1 data-testid='randomizer-resultLabel'>Hungry? Press the button!</h1>
   }
@@ -133,34 +153,38 @@ const Randomizer = ({
     <div data-testid='randomizer' className='randomizer'>
       {nope.active && <h1>NOPE</h1>}
       {selectRestaurantElement()}
-      <div data-testid='foodmodel-container' className='foodmodel' style={{display: `${(restaurant && !isRolling) ? 'none' : 'inline'}`}}>
+      <div data-testid='foodmodel-container' className='foodmodel' style={{ display: `${(restaurant && !isRolling) ? 'none' : 'inline'}` }}>
         <FoodModel rolling={isRolling} />
       </div>
-      <RandomizerButton
-        onClick={startRolling}
-        setError={setError}
-        isPicky={isPicky}
-        isRolling={isRolling}
-        hasResult={restaurant !== undefined && !isRolling}
-        maxNumberOfRolls={maxNumberOfRolls}
-        rollsRemaining={rollsRemaining}
-      />
-      <button
-        className='randomizer-showFilterButton'
-        onClick={() => setFilter({ ...filter, visible: !filter.visible })}>
-        {filter.visible ? 'Hide filter ' : 'Set filter '}
-        {filter.visible ? <ExpandLess /> : <ExpandMore />}
-      </button>
-      <Filter
-        emptyMessage={<strong>#IEatAnything</strong>}
-        filterCategories={filter.categories}
-        setFilterCategories={(value) => setFilter({ ...filter, categories: value })}
-        filterType={filter.type}
-        setFilterType={(value) => setFilter({ ...filter, type: value })}
-        distance={filter.distance}
-        setDistance={(value) => setFilter({ ...filter, distance: value })}
-        showFilter={filter.visible}
-      />
+      {!resultSelected &&
+        <>
+          <RandomizerButton
+            onClick={startRolling}
+            setError={setError}
+            isPicky={isPicky}
+            isRolling={isRolling}
+            hasResult={restaurant !== undefined && !isRolling}
+            maxNumberOfRolls={maxNumberOfRolls}
+            rollsRemaining={rollsRemaining}
+          />
+          <button
+            className='randomizer-showFilterButton'
+            onClick={() => setFilter({ ...filter, visible: !filter.visible })}>
+            {filter.visible ? 'Hide filter ' : 'Set filter '}
+            {filter.visible ? <ExpandLess /> : <ExpandMore />}
+          </button>
+          <Filter
+            emptyMessage={<strong>#IEatAnything</strong>}
+            filterCategories={filter.categories}
+            setFilterCategories={(value) => setFilter({ ...filter, categories: value })}
+            filterType={filter.type}
+            setFilterType={(value) => setFilter({ ...filter, type: value })}
+            distance={filter.distance}
+            setDistance={(value) => setFilter({ ...filter, distance: value })}
+            showFilter={filter.visible}
+          />
+        </>
+      }
     </div>
   )
 }
@@ -189,15 +213,15 @@ const RandomizerButton = ({
       ? 'Rolling!'
       : 'Wait for it...'
     : hasResult
-      ? 'Gimme another one!'
+      ? <span><ThumbDownAlt /> Nope, gimme another one!</span>
       : `I'm feeling ${isPicky ? 'picky' : 'lucky'}!`
 
   return (
     <Button
       data-testid='randomizer-randomizeButton'
       onClick={handleClick}
-      variant='success'
-      size='lg'
+      variant={hasResult ? 'secondary' : 'success'}
+      size={!hasResult && 'lg'}
       disabled={isRolling}>
       {label}
     </Button>
